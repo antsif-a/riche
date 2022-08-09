22d74a298ce1376aa3f281d4fac96832c0fce196
antsif-a <summet.dev@gmail.com>
2022-08-10 00:43:30 +0300

    fix: make ActivityType and ActivityFlags export properly

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ export type { default as Activity } from './api/activity/Activity';
 export type { default as ActivityAssets } from './api/activity/ActivityAssets';
 export type { default as ActivityButton } from './api/activity/ActivityButton';
 export type { default as ActivityEmoji } from './api/activity/ActivityEmoji';
-export type { default as ActivityFlags } from './api/activity/ActivityFlags';
+export { default as ActivityFlags } from './api/activity/ActivityFlags';
 export type { default as ActivityParty } from './api/activity/ActivityParty';
 export type { default as ActivitySecrets } from './api/activity/ActivitySecrets';
 export type { default as ActivityTimestamps } from './api/activity/ActivityTimestamps';
-export type { default as ActivityType } from './api/activity/ActivityType';
+export { default as ActivityType } from './api/activity/ActivityType';
 
 export type { default as Payload } from './api/payload/Payload';
 export type { default as PayloadCommand } from './api/payload/PayloadCommand';
